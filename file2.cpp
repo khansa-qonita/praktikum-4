e62331f2d3d804d49dd471eb000bcf972cfd20c7
khansa-qonita <khansaqonita420@gmail.com>
2026-03-10 18:23:56 +0700

    menambahkan fungsi menentukan status kelulusan

--- file2.cpp
+++ file2.cpp
@@ -4,3 +4,16 @@ using namespace std;
 float Rata_rata(float a, float b){
     return (a+b)/2;
 }
+
+string Status_lulus(float r){
+    if (r >= 60)
+        return "lolos";
+    else if (r >= 50)
+        return "lolos dipertimbangkan";
+    else if (r >= 40)
+        return "lolos tetapi matrikulasi 1 tahun";
+    else
+        return "gagal";
+}
+
+
